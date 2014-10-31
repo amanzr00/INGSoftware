8e3457bf8c467c54fa26223bc696ea85ae06c0e2
pdiezp00 <pablodiezp@gmail.com>
2014-10-31 12:19:11 +0100

    More
    more

--- Thi/src/com/example/thi/MainActivity.java
+++ Thi/src/com/example/thi/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * 
+ * @author Pablo Diez Perez
+ *
+ */
 
 public class MainActivity extends ActionBarActivity {
 
